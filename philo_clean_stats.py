d9cfeeb96e1d58fe3851fae3882651f713018311
EGrandgi <egrandgi@gmail.com>
2020-12-15 22:16:53 +0100

    Add files via upload

--- philo_clean_stats.py
+++ philo_clean_stats.py
@@ -1,47 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
 import pandas as pd
 import numpy as np
 import os
 import csv
 from collections import Counter
-from functions import basic_cleaner
+from functions import basic_cleaner, flat_list
+import unidecode
+import re
 
 dir_tr = os.path.join(os.getcwd(), 'tr')
 os.makedirs(dir_tr, exist_ok=True)
 # dir_out = os.path.join(os.getcwd(), 'out')
 # os.makedirs(dir_out, exist_ok=True)
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
 
 # Get data from philo_scraping
 df = pd.read_csv(os.path.join(dir_tr, 'maphilosophie.csv'), sep='§',
                  encoding='utf-8-sig', escapechar='\\', quoting=csv.QUOTE_NONE)
 
+# Get data from acgrenoble
+df2 = pd.read_csv(os.path.join(dir_tr, 'acgrenoble.csv'), sep='§',
+                  encoding='utf-8-sig', escapechar='\\', quoting=csv.QUOTE_NONE)
+
+
+# concatenate dataframes
+df_all = pd.concat([df, df2], axis=0).reset_index(drop=True)
+
+# correction
+df_all.year = [int(x) if str(x) != 'nan' else '' for x in df_all.year]
+df_all.author = [x if str(x) != 'nan' else '' for x in df_all.author]
+
 # id
-df['id'] = range(df.shape[0])
-df.id = [str(x).zfill(6) for x in df.id]
+df_all['id'] = range(df_all.shape[0])
+df_all.id = [str(x).zfill(6) for x in df_all.id]
 
 # clean content
-df['content_c'] = [basic_cleaner(s) for s in df.content]
+df_all['content_c'] = [basic_cleaner(s) for s in df_all.content]
 
 # stats
-df['nchar'] = [len(s) for s in df.content_c]
-df['nwords'] = [s.count(' ') for s in df.content_c]
-df['nsent'] = [s.count('.') + s.count('!') + s.count('?') for s in df.content_c]
-df['nint'] = [s.count('?') for s in df.content_c]
-df['nexcl'] = [s.count('!') for s in df.content_c]
-df['ncomm'] = [s.count(',') + s.count(';') for s in df.content_c]
+df_all['nchar'] = [len(s) for s in df_all.content_c]
+df_all['nwords'] = [s.count(' ') for s in df_all.content_c]
+df_all['nsent'] = [s.count('.') + s.count('!') + s.count('?')
+                   for s in df_all.content_c]
+df_all['nsent'] = [s if s > 0 else 1 for s in df_all.nsent]
+df_all['nint'] = [s.count('?') for s in df_all.content_c]
+df_all['nexcl'] = [s.count('!') for s in df_all.content_c]
+df_all['ncomm'] = [s.count(',') + s.count(';') for s in df_all.content_c]
 
 # convert to int
 for var_ in ['nchar', 'nwords', 'nsent', 'nint', 'nexcl', 'ncomm']:
-    df[var_] = [int(x) for x in df[var_]]
-    
+    df_all[var_] = [int(x) for x in df_all[var_]]
+
 # ratios
-df['rint'] = [np.round(x / y, 4) for x, y in zip(df.nint, df.nchar)]
-df['rexcl'] = [np.round(x / y, 4) for x, y in zip(df.nexcl, df.nchar)]
-df['wordssent'] = [np.round(y / x, 2) for x, y in zip(df.nsent, df.nwords)]
+df_all['rint'] = [np.round(x / y, 4)
+                  for x, y in zip(df_all.nint, df_all.nchar)]
+df_all['rexcl'] = [np.round(x / y, 4)
+                   for x, y in zip(df_all.nexcl, df_all.nchar)]
+df_all['rwords'] = [np.round(y / x, 2)
+                    for x, y in zip(df_all.nsent, df_all.nwords)]
+
+# authors
+unique_authors = np.unique(df_all.author)
+unique_authors = [x for x in unique_authors if x != '']
+df_corresp_auth = pd.DataFrame(columns=['author', 'author_full'])
+df_corresp_auth.author = unique_authors
+df_corresp_auth.author_full = [[x for x in unique_authors if a in x and '(' in x and '(' + a + ')' not in x][0] if len(
+    [x for x in unique_authors if a in x and '(' in x and '(' + a + ')' not in x]) > 0 else a if '(' not in a and ')' not in a else a for a in unique_authors]
+df_corresp_auth.author_full = [re.sub("[^A-Za-z.!?,; ]+", '', unidecode.unidecode(
+    s.replace('\x92', ' '))).replace(' ', '_') for s in df_corresp_auth.author_full]
+df_corresp_auth.author_full = ['Cournot_AntoineAugustin' if x ==
+                               'Cournot_Antoine_Augustin' else x for x in df_corresp_auth.author_full]
+df_corresp_auth.loc[df_corresp_auth.author ==
+                    'Augustin', 'author_full'] = 'Augustin'
+df_corresp_auth.loc[df_corresp_auth.author ==
+                    'Hegel (Georg Wilhelm Friedrich)', 'author_full'] = 'Hegel_Georg_Wilhelm_F.'
+df_all = pd.merge(df_all, df_corresp_auth, left_on='author',
+                  right_on='author', how='outer')
+
+
 
 
 # TO BE CONTINUED
-
 
 
 # Save
--- philo_scraping.py
+++ philo_scraping.py
@@ -5,9 +5,12 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import numpy as np
 import os
 import csv
-from functions import create_soup
+from functions import create_soup, flat_list
+import unidecode
+import re
 
 dir_tr = os.path.join(os.getcwd(), 'tr')
 os.makedirs(dir_tr, exist_ok=True)
@@ -32,24 +35,160 @@ for td in soup.find_all('td', class_='cellules'):
 
 # Store content in a dataframe
 df = pd.DataFrame(columns=['source', 'link', 'author',
-                           'title', 'subtitle', 'year', 'content'])
+                           'title', 'theme', 'year', 'content'])
 df.link = links
 df.source = 'maphilosophie.fr'
 
 for k in df.index:
     link = df.loc[k, 'link']
     soup = create_soup(link, 'cp1252')
-    df.loc[k, 'author'] = soup.find('div', class_='col-lg-12').find('h1').text
-    df.loc[k, 'subtitle'] = soup.find(
+    author = soup.find('div', class_='col-lg-12').find('h1').text
+    author = author[:len(author) - 1] if author[len(author) -
+                                                1:len(author)] == ' ' else author
+    df.loc[k, 'author'] = author
+    author_short = author.split('(')[0]
+    author_short = author_short[:len(author_short) - 1] if author_short[len(
+        author_short) - 1:len(author_short)] == ' ' else author_short
+    df.loc[k, 'author_short'] = author_short
+    df.loc[k, 'theme'] = soup.find(
         'div', class_='col-lg-12').find('h2').text
     info = soup.find('div', class_='ref').text.split(',')
     df.loc[k, 'title'] = info[1] if len(info) > 0 else ''
-    df.loc[k, 'year'] = info[len(info) - 1] if len(info) > 0 else ''
+
+    year = info[len(info) - 1] if len(info) > 0 else ''
+    year = re.findall('[1-3][0-9]{3}', year)
+    year = ['0'] if year == [] else year
+    year = min([int(x) for x in year])
+    df.loc[k, 'year'] = year
+
     df.loc[k, 'content'] = soup.find('div', class_='corps').text.replace(
         '\x92', "'").replace('\r', '').replace('\n', '')
+
+
+# year
+df.year = [x if x != 0 else '' for x in df.year]
+
+
+# theme
+df.theme = [unidecode.unidecode(
+    x.replace('\x92', ' ').lower()) for x in df.theme]
+df.theme = [re.sub("[^A-Za-z -']+", '', x) for x in df.theme]
+df.theme = [x.replace(' - ', '|').replace(' ', '|').replace('/', '|').replace('&', '').replace('||', '|').replace("l'", '').replace('|de|', '|').replace(
+    '|la|', '|').replace('|le|', '|').replace('|du|', '|').replace('\r', '').replace('\n', '').replace('(s', '').replace(')', '').replace("l'", '') for x in df.theme]
+df.theme = [[z for z in x.split('|') if z not in ['la', 'le', 'un', 'une', 'les', 'des', 'du', 'de', 'et', 'en', 'quoi',
+                                                  'sans', '?', '!', '', 'a', '(2)', 'est', 'que', 'ce', 'dans', 'nos', 'comme', 'il', 'ii', 'aux', 'par']] for x in df.theme]
+df.theme = [[z.replace('"', '')
+             for z in x if "'" not in z and '-' not in z] for x in df.theme]
+df.theme = ['|'.join(x) for x in df.theme]
+
+
+# drop
+inds = [i for i, x in zip(df.index, df.author_short) if 'monde' in x.lower()]
+df = df.drop(inds, axis=0).reset_index(drop=True)
 
 # Save
 website = 'maphilosophie'
 df.to_csv(os.path.join(dir_tr, f'{website}.csv'), sep='§', index=False,
           encoding='utf-8-sig', escapechar='\\', quoting=csv.QUOTE_NONE)
+
+
+
+# ## http://www.ac-grenoble.fr/PhiloSophie/old2/bases/search.php
+
+
+# Get list of authors from maphilosophie
+authors_list = np.unique(df.author_short)
+df = df.drop(['author_short'], axis=1)
+authors_list = [unidecode.unidecode(x.lower()).replace(
+    ' ', '+').replace("'", '%27').replace('-', '+') for x in authors_list if x != '']
+authors_list.sort()
+links = [
+    f'http://www.ac-grenoble.fr/PhiloSophie/old2/bases/search.php?auteur={a}&texte=&reference=&theme=' for a in authors_list]
+
+# Prepare dataframe
+df2 = pd.DataFrame(columns=['source', 'link', 'author',
+                            'title', 'theme', 'year', 'content'])
+
+auth_vect = []
+cont_vect = []
+ref_vect = []
+th_vect = []
+year_vect = []
+link_vect = []
+
+
+for link in links:
+
+    soup = create_soup(link, 'cp1252')
+
+    # get content
+    author = [span.text for span in soup.find_all(
+        'span', class_='sstitremarron')]
+    content = [span.find('div', {'style': 'text-align: justify;'}).text.replace(
+        '\x92', "'").replace('\r', '').replace('\n', '') for span in soup.find_all(
+        'span', class_='texte') if span.find('div', {'style': 'text-align: justify;'}) is not None]
+    reference = [span.find('div', {'style': 'text-align: right;'}).text.replace(
+        '\x92', "'").replace('\r', '').replace('\n', '') for span in soup.find_all(
+        'span', class_='texte') if span.find('div', {'style': 'text-align: right;'}) is not None]
+    themes = [td.find('b').text for td in soup.find_all(
+        'td', {'align': 'right'})]
+    years = [re.findall('[1-3][0-9]{3}', s) for s in reference]
+
+    # append to list
+    auth_vect.append(author)
+    cont_vect.append(content)
+    ref_vect.append(reference)
+    th_vect.append(themes)
+    year_vect.append(years)
+    link_vect.append([link] * len(themes))
+
+
+# flatten
+auth_vect = flat_list(auth_vect)
+cont_vect = flat_list(cont_vect)
+ref_vect = flat_list(ref_vect)
+th_vect = flat_list(th_vect)
+year_vect = flat_list(year_vect)
+link_vect = flat_list(link_vect)
+
+# year
+year_vect = [['0'] if x == [] else x for x in year_vect]
+year_vect = [min([int(z) for z in x]) for x in year_vect]
+year_vect = [x if x != 0 else '' for x in year_vect]
+
+print(f'auth_vect: {len(auth_vect)}')
+print(f'cont_vect: {len(cont_vect)}')
+print(f'ref_vect: {len(ref_vect)}')
+print(f'th_vect: {len(th_vect)}')
+print(f'year_vect: {len(year_vect)}')
+print(f'link_vect: {len(link_vect)}')
+
+
+df2['link'] = link_vect
+df2['author'] = auth_vect
+df2['title'] = ref_vect
+df2['theme'] = th_vect
+df2['content'] = cont_vect
+df2['year'] = year_vect
+df2['source'] = 'ac-grenoble.fr'
+
+# theme
+df2.theme = [unidecode.unidecode(
+    x.replace('\x92', ' ').lower()) for x in df2.theme]
+df2.theme = [re.sub("[^A-Za-z -']+", '', x) for x in df2.theme]
+df2.theme = [x.replace(' - ', '|').replace(' ', '|').replace('/', '|').replace('&', '').replace('||', '|').replace("l'", '').replace('|de|', '|').replace(
+    '|la|', '|').replace('|le|', '|').replace('|du|', '|').replace('\r', '').replace('\n', '').replace('(s', '').replace(')', '').replace("l'", '') for x in df2.theme]
+df2.theme = [[z for z in x.split('|') if z not in ['la', 'le', 'un', 'une', 'les', 'des', 'du', 'de', 'et', 'en', 'quoi',
+                                                  'sans', '?', '!', '', 'a', '(2)', 'est', 'que', 'ce', 'dans', 'nos', 'comme', 'il', 'ii', 'aux', 'par']] for x in df2.theme]
+df2.theme = [[z.replace('"', '')
+             for z in x if "'" not in z and '-' not in z] for x in df2.theme]
+df2.theme = ['|'.join(x) for x in df2.theme]
+
+
+# Save
+website = 'acgrenoble'
+df2.to_csv(os.path.join(dir_tr, f'{website}.csv'), sep='§', index=False,
+           encoding='utf-8-sig', escapechar='\\', quoting=csv.QUOTE_NONE)
+
+
 
