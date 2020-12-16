06e111240bbd73064118f964b85bd0c729d87131
EGrandgi <egrandgi@gmail.com>
2020-12-16 19:43:45 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -8,15 +8,32 @@ import pandas as pd
 import csv
 import re
 import unidecode
-
+from gensim.utils import simple_preprocess
+import gensim
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = stopwords.words('french')
+import spacy
+nlp = spacy.load('fr_core_news_md')
 
 
 # =============================================================================
-#                                 Functions
+#                                 Utils
 # =============================================================================
 
 
-def create_soup(url, enc):
+def flat_list(l: list) -> list:
+
+    return [x for sub in l for x in sub]
+
+
+# =============================================================================
+#                                 Scraping
+# =============================================================================
+
+
+def create_soup(url: str, enc: str):
     """ 
         In: url
         Out: source code  
@@ -29,7 +46,12 @@ def create_soup(url, enc):
     return soup
 
 
-def basic_cleaner(s, lower):
+# =============================================================================
+#                                 NLP
+# =============================================================================
+
+
+def basic_cleaner(s: str, lower: bool) -> str:
 
     s = re.sub("[^A-Za-z.!?,;' ]+", '', unidecode.unidecode(s.replace('(...)',
                                                                       '').replace('[...]', '').replace('\x92', ' ').replace('\x9c', 'oe'))).replace("'", ' ').replace('  ', ' ')
@@ -38,6 +60,32 @@ def basic_cleaner(s, lower):
     return s
 
 
-def flat_list(l: list) -> list:
+def tokenize(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
 
-    return [x for sub in l for x in sub]
+
+def remove_stopwords(texts: list) -> list:
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append(
+            [token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+def clean_lemmatize(data: list):
+    data_tokens_nostops = remove_stopwords(data)
+    data_tokens_bigrams = make_bigrams(data_tokens_nostops)
+    data_lemmas_bigrams = lemmatization(data_tokens_bigrams, allowed_postags=[
+                                        'NOUN', 'ADJ', 'VERB', 'ADV'])
+    return data_tokens_bigrams, data_lemmas_bigrams
